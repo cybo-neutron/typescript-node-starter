a2cfa1616bca7fe5da0ab11d4770df0c6b4c9887
cybo-neutron <niranjan.pandit@emitrr.com>
2024-06-22 09:10:23 +0530

    use multiple env files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world");
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+const { NODE_ENV } = process.env;
+dotenv.config({
+  path:
+    NODE_ENV === "local"
+      ? path.join(__dirname, "..", ".env.local")
+      : path.join(__dirname, "..", ".env.prod"),
+});
